fbc51ec1485a8edd1c2382bf519d69f8aeb7b0f3
narendra <kolli7571@gmail.com>
2023-11-15 15:52:18 +0530

    added code for Apache active MQ classic scheduler support

--- src/main/java/com/bridgingcode/springbootactivemqdemo/publisher/controller/PublishController.java
+++ src/main/java/com/bridgingcode/springbootactivemqdemo/publisher/controller/PublishController.java
@@ -2,6 +2,9 @@ package com.bridgingcode.springbootactivemqdemo.publisher.controller;
 
 import com.bridgingcode.springbootactivemqdemo.model.SystemMessage;
 //import org.apache.activemq.ScheduledMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.activemq.ScheduledMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,37 +15,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.jms.*;
+
 /**
- *
  * @author AJ Catambay of Bridging Code
- *
  */
 @RestController
 public class PublishController {
-Logger logger= LoggerFactory.getLogger(PublishController.class);
+    Logger logger = LoggerFactory.getLogger(PublishController.class);
     @Autowired
     private JmsTemplate jmsTemplate;
 
+
     @PostMapping("/publishMessage")
-    public ResponseEntity<String> publishMessage(@RequestBody SystemMessage systemMessage) {
-        try {
-            jmsTemplate.setDeliveryDelay(1000);
-            logger.info("Messsage recived");
-//            for (int i = 0; i < 30; i++) {
-//                systemMessage.setMessage(systemMessage.getMessage().concat(Integer.toString()));
-            jmsTemplate.convertAndSend("bridgingcode-queue", systemMessage,postProcessor->{
-                postProcessor.setLongProperty("AMQ_SCHEDULED_DELAY",100000);
-//                postProcessor.setLongProperty("AMQ_SCHEDULED_PERIOD",100000);
-                postProcessor.setLongProperty("AMQ_SCHEDULED_REPEAT",3);
-                return postProcessor;
-            });
-//            jmsTemplate.convertAndSend("bridgingcode-queue",systemMessage);
-//            }
-
-            return new ResponseEntity<>("Sent.", HttpStatus.OK);
-
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<String> publishMessage(@RequestBody SystemMessage systemMessage) throws JMSException, JsonProcessingException {
+        Connection connection = this.jmsTemplate.getConnectionFactory().createConnection();
+        Session session = connection.createSession();
+        Queue queue = session.createQueue("bridgingcode-queue");
+        MessageProducer producer = session.createProducer(queue);
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMessage message = session.createObjectMessage(systemMessage);
+        message.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_DELAY, 1000);
+        message.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_PERIOD, 1000);
+        message.setIntProperty(ScheduledMessage.AMQ_SCHEDULED_REPEAT, 9);
+        producer.send(message);
+        return new ResponseEntity<>("Sent.", HttpStatus.OK);
     }
 }
